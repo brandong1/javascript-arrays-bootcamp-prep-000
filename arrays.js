a7c0353b0cc81127874394c5766217dfaef8424e
Brandon Green <brandon@brandonleeproductions.com>
2018-04-15 04:05:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   }
 
 function addElementToEndOfArray(array, element) {
-  addElementToEndOfArray([1], element)
+  addElementToEndOfArray.push([], element)
   return array;
 }
 
